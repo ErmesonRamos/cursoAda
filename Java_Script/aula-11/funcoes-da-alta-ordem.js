03f8305091ee6921e26c0a4f3b7a46b687a137a1
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-09-15 23:28:13 -0300

    funções de alta ordem completo

--- Java_Script/aula-11/funcoes-da-alta-ordem.js
+++ Java_Script/aula-11/funcoes-da-alta-ordem.js
@@ -36,13 +36,40 @@
         console.log(result);
   ! Funções de alta ordem de arrays
     -> Quando aplicamos esse conceito a arrays, estamos falando sobre funções que operam em arrays e utilizam outras funções para transformar ou processar os elementos do array.
-    ? 1.array.forEach(itens dentro do array(elementos), indice dos nossos elementos(index), ele mostra tudo o array (arrayCompleto) );
-        -> em JavaScript é uma função de alta ordem usada para iterar sobre os elementos de um array. Ele aplica uma função de callback a cada elemento do array em ordem, mas, ao contrário de alguns outros métodos de iteração, forEach() não retorna um novo array e não altera o array original. Em vez disso, ele é usado para executar um código em cada item do array.
-        * Exemplo:
-          const numbers = [1, 2, 3, 4, 5, 6, 7];
-          numbers.forEach((element) => {
-            console.log(element);
-          });
+    ? 1.array.forEach(itens dentro do array(elementos), indice dos elementos a qual ele está percorrendo(index), ele mostra tudo o array (arrayCompleto));
+      -> em JavaScript é uma função de alta ordem usada para iterar sobre os elementos de um array. Ele aplica uma função de callback a cada elemento do array em ordem, mas, ao contrário de alguns outros métodos de iteração, forEach() não retorna um novo array e não altera o array original. Em vez disso, ele é usado para executar um código em cada item do array.
+      * Exemplo:
+        const numbers = [1, 2, 3, 4, 5, 6, 7];
+        numbers.forEach((element) => {
+          console.log(element);
+        });
+    ? 2. array.find(itens dentro do array(item), indice dos itens a qual ele está percorrendo(index), ele mostra tudo o array (arrayCompleto))
+      -> em Javascript é util para encontrar um elemento detro de um array, ele necessecita de uma condição para buscar os elementos
+      * Exemplo:
+        const persons = [
+      {
+        name: 'Pessoa1',
+        age: 25
+      },
+
+      {
+        name: 'Pessoa2',
+        age: 30
+      },
+
+      {
+        name: 'Pessoa3',
+        age: 15
+      }
+    ]
+
+    const personFound = persons.find((persons) => {
+      return persons.age === 15;
+    });
+
+    console.log(personFound);
+  ? array.findIndex()
+    -> Em JavaScript é um método que retorna o índice do primeiro elemento do array que satisfaz a função fornecida. Caso nenhum elemento seja encontrado, retorna -1.
 
       
 */
--- Java_Script/aula-11/tempCodeRunnerFile.js
+++ Java_Script/aula-11/tempCodeRunnerFile.js
@@ -1,5 +1,25 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7];
+const persons = [
+  {
+    name: 'Pessoa1',
+    age: 25,
+    heigth: 1.70
+  },
 
-numbers.forEach((element, index, arrayComplet) => {
-  console.log(element, index,  arrayComplet);
+  {
+    name: 'Pessoa2',
+    age: 30,
+    heigth: 1.60
+  },
+
+  {
+    name: 'Pessoa3',
+    age: 15,
+    heigth: 1.90
+  }
+]
+
+const indexFromPersonFound = persons.findIndex((persons) => {
+  return persons.age === 15 && persons.heigth < 1.70;
 });
+
+console.log(indexFromPersonFound);
