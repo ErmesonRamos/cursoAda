185ee56587d577722357e789fe8452c8f77efeb5
Jeferson Bryan <samarasales2556@gmail.com>
2024-11-27 14:45:23 -0300

    APIs

---
+++ Java_Script/aula-15/APIs.js
@@ -0,0 +1,22 @@
+/*
+  ! APIs
+    -> Em potuguês, Interface de programação de Aplicações. 
+    -> Hoje em dia é muito comum que diferentes aplicações consumam dados pela internet, muitas vezes dados providenciados por terceiros.
+    -> Por exemplo, um aplicativo de entrega de alimentos pode usar dados de geolocalização do Google para localizar restaurantes próximos ao usário e exibir a rota percorrida pelo entregador.
+    -> Como esseas aplicações podem estar rodando em diferentes plataformas e linguagens de programação, é importante estabelecer uma "Liguagem comum" para que todos consigam esses dados.
+    -> A API é uma interface que nos permite utilizar funcionalidades/ recursos de terceiros, seguindo um conjuto de definições/ contratos estabelecidos pelo terceiro.
+    * Exemplos:
+      -> Login social
+      -> Pagamento Online
+    -> A API acaba facilitando a vida dos programadores, pois reduz o tempo de desenvolvimento de novas aplicações.
+    * Tipos de APIs:
+      1.Públicas ou abertas
+      2. Privadas ou internas
+      3. APIs de Parceiras
+    * Protocolos de APIs:
+      -> Os protocolos servem para padronizar a troca de dados entre a aplicação e a API.
+      -> Existem diferentes protocolos de APIs, como RPC, SOAP, GraphQL e REST.
+      -> O REST é o padrão mais comum.
+      -> REST (Representational State Transfer) se baseia em seis restrições. Caso uma API respeite essas 6 restrições, ela é considerada uma API RESTful.
+*/
+ 
