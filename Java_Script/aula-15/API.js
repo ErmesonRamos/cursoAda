9f627ccdc7f43276990714e62ebfab4a91eb209b
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-11-30 01:02:03 -0300

    3 protocolos RESTful.

--- Java_Script/aula-15/API.js
+++
@@ -1,5 +0,0 @@
-/*
-  ! APIs
-    ->Em português, Interface de Programação de aplicações.
-    ->Hoje em dia é muito comum que diferentes aplicações con
-*/
--- Java_Script/aula-15/APIs.js
+++ Java_Script/aula-15/APIs.js
@@ -3,7 +3,7 @@
     -> Em potuguês, Interface de programação de Aplicações. 
     -> Hoje em dia é muito comum que diferentes aplicações consumam dados pela internet, muitas vezes dados providenciados por terceiros.
     -> Por exemplo, um aplicativo de entrega de alimentos pode usar dados de geolocalização do Google para localizar restaurantes próximos ao usário e exibir a rota percorrida pelo entregador.
-    -> Como esseas aplicações podem estar rodando em diferentes plataformas e linguagens de programação, é importante estabelecer uma "Liguagem comum" para que todos consigam esses dados.
+    -> Como essas aplicações podem estar rodando em diferentes plataformas e linguagens de programação, é importante estabelecer uma "Liguagem comum" para que todos consigam esses dados.
     -> A API é uma interface que nos permite utilizar funcionalidades/ recursos de terceiros, seguindo um conjuto de definições/ contratos estabelecidos pelo terceiro.
     * Exemplos:
       -> Login social
@@ -18,5 +18,9 @@
       -> Existem diferentes protocolos de APIs, como RPC, SOAP, GraphQL e REST.
       -> O REST é o padrão mais comum.
       -> REST (Representational State Transfer) se baseia em seis restrições. Caso uma API respeite essas 6 restrições, ela é considerada uma API RESTful.
+    * Protocolos de APIs:
+      -> Ter uma arquitetura formada por clientes, servidores e recursos, com solicitações gerenciadas por HTTP.
+      -> Estabelecer uma comunicação stateless entre cliente e servidor. Isso significa que nenhuma informação do cliente é armazenada entre solicitações GET e todas as solicitações são separadas e desconectadas.
+      -> Armazenar dados em cache para otimizar as interações entre cliente e servidor. 
 */
  
