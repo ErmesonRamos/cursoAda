1d65f3f50d62e5e64af9ba25cf353e921dba0501
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-12-03 22:55:24 -0300

    Vendo um pouco sobre a API de dicionario

--- Java_Script/aula-15/APIs.js
+++ Java_Script/aula-15/APIs.js
@@ -1,6 +1,6 @@
 /*
   ! APIs
-    -> Em potuguês, Interface de programação de Aplicações. 
+    -> Em português, Interface de programação de Aplicações. 
     -> Hoje em dia é muito comum que diferentes aplicações consumam dados pela internet, muitas vezes dados providenciados por terceiros.
     -> Por exemplo, um aplicativo de entrega de alimentos pode usar dados de geolocalização do Google para localizar restaurantes próximos ao usário e exibir a rota percorrida pelo entregador.
     -> Como essas aplicações podem estar rodando em diferentes plataformas e linguagens de programação, é importante estabelecer uma "Liguagem comum" para que todos consigam esses dados.
@@ -13,11 +13,13 @@
       1.Públicas ou abertas
       2. Privadas ou internas
       3. APIs de Parceiras
+
     * Protocolos de APIs:
       -> Os protocolos servem para padronizar a troca de dados entre a aplicação e a API.
       -> Existem diferentes protocolos de APIs, como RPC, SOAP, GraphQL e REST.
       -> O REST é o padrão mais comum.
       -> REST (Representational State Transfer) se baseia em seis restrições. Caso uma API respeite essas 6 restrições, ela é considerada uma API RESTful.
+
     * Protocolos de APIs:
       -> Ter uma arquitetura formada por clientes, servidores e recursos, com solicitações gerenciadas por HTTP.
       -> Estabelecer uma comunicação stateless entre cliente e servidor. Isso significa que nenhuma informação do cliente é armazenada entre solicitações GET e todas as solicitações são separadas e desconectadas.
@@ -43,8 +45,11 @@
 
     -> Endpoint: A URL completa que deve ser acessado a fim de obter um recurso específico.
       a. Dictionary API:
-        https://api.dictionaryapi.dev/api/v2/entries/en/hello.
+        https://api.dictionaryapi.dev/api/v2/entries/en/hello.(GET)
       b. OpenWeather API:
         https://api.openweathermap.org/data/2.5/weather?last={lat}&lon={lon}&appid={API key}.
+    * Exemplos de APIs de terceiros
+        1.PokéAPI
+        2.
 */
 
