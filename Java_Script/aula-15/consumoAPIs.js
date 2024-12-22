cf5e82f5bae05198002bcc3f1872e4c4370f30f4
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-12-22 20:58:41 -0300

    then e catch / async e await

---
+++ Java_Script/aula-15/consumoAPIs.js
@@ -0,0 +1,26 @@
+/*
+  ! Fetch API
+  fetch('URL', )
+    * Para quê serve a FetchAPI
+        Fetch API é uma API do Browser (Ela basicamente serve para pegar dados da API).
+  
+*/
+
+// then / catch
+fetch("https://viacep.com.br/ws/01001000/json/")
+  .then((response) => {
+    response.json().then((datas) => console.log(datas));    
+  })
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+// async / await
+
+async function getDatasFromCep() {
+  const response = await fetch("https://viacep.com.br/ws/01001000/json/");
+  const datas = await response.json();
+  console.log(datas);
+}
+
+getDatasFromCep();
