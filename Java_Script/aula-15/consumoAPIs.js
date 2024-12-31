ac3411e70d96f2f73943337c10b25e527dcf0858
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-12-31 01:52:32 -0300

    Terminei API!

--- Java_Script/aula-15/consumoAPIs.js
+++ Java_Script/aula-15/consumoAPIs.js
@@ -1,8 +1,27 @@
 /*
   ! Fetch API
-  fetch('URL', )
+  fetch('URL', 'heards body method' ) 
     * Para quê serve a FetchAPI
         Fetch API é uma API do Browser (Ela basicamente serve para pegar dados da API).
+    * Exemplo
+        async function createUser() {
+          const userData = {
+            'firstName': 'Ermeson',
+            'lastName': 'Ramos',
+            'email': 'ermeson@test.com'
+          }
+            
+          await fetch('https://viacep.com.br/ws/01001000/json/', {
+          method: 'POST',
+          headers: {
+            'app-id': '63f77cc1b7f489f0b351b30f', -> Chave da minha API
+            'Content-Type': 'application/json', 
+          },
+          body: JSON.strify(userData), -> O body não aceita objeto é necessario utilizar o JSON.strinfy(variavel)
+        })
+        }
+        
+       
   
 */
 
