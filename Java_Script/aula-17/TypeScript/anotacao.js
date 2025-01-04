48f0567e47c4aa28751afea0a58ce16715b9ec0f
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-01-04 03:00:14 -0300

    Inicializando em TypeScript

---
+++ Java_Script/aula-17/TypeScript/anotacao.js
@@ -0,0 +1,13 @@
+/*
+  ! TypeScript 
+    -> Nada mas é que do que o JavaScript com a adição de tipagem estatíca.
+    * Por que estudar o TypeScript
+      -> Types.
+      -> Utilização de um JS mais recente (Compilado para funcionar em browsers mais antigos).
+      -> Adiciona recursos, que não existem no JS (ex: Interfaces e Generics).
+      -> Muitas opções de configurações.
+      -> Melhores sugestão de codígo em IDEs que têm um suporte melhor ao TS.
+    * Como utilizar o TypeScript
+      -> O TypeScript é configurado com o node.js
+*/
+
---
+++ Java_Script/aula-17/TypeScript/index.ts
@@ -0,0 +1,3 @@
+const num1:number = 1;
+const num2:number = 2;
+console.log(num1 + num2);
