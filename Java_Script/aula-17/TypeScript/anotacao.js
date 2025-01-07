822fd1c6ce51c25bf29e883ab5dcdaeb58c66ce2
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-01-06 23:33:49 -0300

    TypeScript I

--- Java_Script/aula-17/TypeScript/anotacao.js
+++ Java_Script/aula-17/TypeScript/anotacao.js
@@ -9,5 +9,15 @@
       -> Melhores sugestão de codígo em IDEs que têm um suporte melhor ao TS.
     * Como utilizar o TypeScript
       -> O TypeScript é configurado com o node.js
+    * Configurações do TypeScript 
+      1. Inicializar um projeto Node.js 
+      1.1. `nmp init ou -y`
+      2. Instalar o TypeScript 
+        2.1. Instalação Global (Instala o TypeScript na máquina toda). -> `npm install -g typescript`
+        2.2. Instalação Local (Instalção dentro do projeto). -> `npm install typescript -D`
+      3. Utilizar o TypeScript para transpilar o nosso codígo 
+      3.1. `npx tsc 'nomedoarquivoparasercompliado --watch'`
+      4. Mexer nas configurações do TypeScript
+        4.1 `npx tsc --init`
 */
 
