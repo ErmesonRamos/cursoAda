ca38de9a9d5d41979b6f0b89f08e7de77d0e3219
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-01-18 22:56:46 -0300

    DOM - Document Object Model

---
+++ Front-end Dinâmico/js/index.js
@@ -0,0 +1,40 @@
+// Selecionar elemetos HTML da nossa página utilizando o document.
+
+// 1. Pelo nome da Tag
+const allP = document.getElementsByTagName('p');
+console.log(allP);
+
+// 2. Pelo nome da Classe
+const allClasseParagraph = document.getElementsByClassName('paragraph');
+console.log(allClasseParagraph);
+
+// 3. Pelo name da Tag
+const emailInput = document.getElementsByName('email');
+console.log(emailInput);
+
+// 4. Pelo id
+const jsLogoImg = document.getElementById('js-logo');
+console.log(jsLogoImg);
+
+// 5. query Select
+const imagem = document.querySelector('body > img#js-logo');
+console.log(imagem);
+
+// 6. query SelectorAll
+const paragraph = document.querySelectorAll('p');
+console.log(paragraph);
+
+
+// Alterar o texto dos elementos HTML
+console.clear();
+const primaryParagraph = document.querySelector('p.paragraph');
+console.log("textContent:",primaryParagraph.textContent);
+primaryParagraph.textContent = 'Alterado pelo JavaScript!';
+
+console.log("innerHTML: ",primaryParagraph.innerHTML);
+primaryParagraph.innerHTML = '<strong> Negrito </strong>';
+
+
+console.clear();
+emailInput[0].value = 'Ermeson';
+
