1d2c112f0e850b0e2a6812fc0494480cc55e6018
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-01-22 02:17:31 -0300

    listen events

--- Front-end Dinâmico/aula-21/js/index.js
+++ Front-end Dinâmico/aula-21/js/index.js
@@ -1,5 +1,13 @@
-const button = document.querySelector('section button');
+const deacreaseButton = document.querySelector('#deacrease-button');
+const increaseButton = document.querySelector('#increase-button');
+const counter = document.querySelector('#counter');
 
-button.addEventListener('click', () => {
-  
+deacreaseButton.addEventListener('click', () => {
+  const valueCurrent = Number(counter.textContent) - 1;
+  counter.textContent = valueCurrent;
+});
+
+increaseButton.addEventListener('click', () => {
+  const valueCurrent = Number(counter.textContent) + 1;
+  counter.textContent = valueCurrent;
 });
