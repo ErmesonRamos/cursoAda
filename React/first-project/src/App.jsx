67364fa182add9932bc15a8cb20ffda9ac2b9db9
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-02-08 22:28:54 -0300

    Navbar complete

--- React/first-project/src/App.jsx
+++ React/first-project/src/App.jsx
@@ -1,21 +1,12 @@
-import React from "react"
+import React from "react";
+import Navbar from "./assets/Navbar/Navbar";
+import './assets/styles/App.css'
 // Componente em classe é uma classe que herda a classe Componentes do React, e retorna HTML dentro método render.
 class App extends React.Component {
-  constructor() {
-    super()
-    this.name = 'Ermeson Ramos'
-  }
+
   // Método responsável por renderizar o contéudo HTML do nosso componente.
    render() {
-    return (
-      <>
-        <div>
-          <h1>Aprendendo React</h1>
-          <h2>{this.name}</h2>
-          <p>Isso aqui é um parágrafo</p>
-        </div>
-      </>
-    )
+    return <Navbar />;
   }
 }
 
