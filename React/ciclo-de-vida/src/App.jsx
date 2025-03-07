ffa3593283b1567e3edc4c3dafbd17c0b6154d83
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2025-03-06 21:51:59 -0300

    cicly life

---
+++ React/ciclo-de-vida/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+class App extends React.Component {
+  
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Ciclo de Vida no React</h1>
+          <button>Mostrar componente</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App
